8bde6f3f3dc1eaae824fc66dd6a5ef486efab4c5
taihe-a <taihei618@gmail.com>
2020-04-03 12:03:56 +0900

    rename_file削除

--- db/migrate/20200402025323_rename_details_column_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class RenameDetailsColumnToTasks < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :tasks, :details, :detail
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_025323) do
+ActiveRecord::Schema.define(version: 2019_11_11_085356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
