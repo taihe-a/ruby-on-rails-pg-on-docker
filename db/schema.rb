6514c2ce3be3bb65d2fc009b6c4817a670221c8a
taihe-a <taihei618@gmail.com>
2020-04-06 23:16:12 +0900

    indexを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_022416) do
+ActiveRecord::Schema.define(version: 2020_04_06_132850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_04_06_022416) do
     t.datetime "updated_at", null: false
     t.date "deadline"
     t.integer "progress"
+    t.index ["name"], name: "index_tasks_on_name"
   end
 
   create_table "users", force: :cascade do |t|
