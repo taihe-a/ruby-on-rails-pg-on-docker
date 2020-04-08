5946cbf32dc8469552a0b4a37c14b12befb065ec
taihe-a <taihei618@gmail.com>
2020-04-08 10:19:42 +0900

    userモデルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_406_132_850) do
+ActiveRecord::Schema.define(version: 2020_04_08_011437) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'tasks', force: :cascade do |t|
-    t.string 'name', null: false
-    t.text 'detail'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.date 'deadline'
-    t.integer 'progress'
-    t.index ['name'], name: 'index_tasks_on_name'
+  create_table "tasks", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "detail"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.date "deadline"
+    t.integer "progress"
+    t.integer "priority"
+    t.index ["name"], name: "index_tasks_on_name"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'email', null: false
-    t.string 'password', null: false
-    t.string 'password_digest', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
