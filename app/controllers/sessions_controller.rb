0f1dde2ca67026b92c4db071f08e7e51a34b6ec2
taihe-a <taihei618@gmail.com>
2020-04-09 16:53:07 +0900

    sessionクラスのcreatemethod作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    binding.pry
+    user = User.find_by(email: session_params[:email])
   end
   
   def destory
     
   end
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+  
 end
