0ff8ded347900e67225d086168fb9adb9c52b076
taihe-a <taihei618@gmail.com>
2020-04-09 10:10:51 +0900

    rubocop

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,19 +5,19 @@ RSpec.describe Task, type: :model do
     let(:fixed_task) { FactoryBot.create(:fixed_task) }
 
     describe 'valid' do
-      it "有効なデータであること" do
+      it '有効なデータであること' do
         expect(fixed_task).to be_valid
       end
     end
 
     describe 'invalid' do
-      it "nameが空の場合" do
+      it 'nameが空の場合' do
         fixed_task.name = ''
         expect(fixed_task).to be_invalid
       end
 
-      it "detailが10文字以下の場合" do
-        fixed_task.detail = "アイウエオ"
+      it 'detailが10文字以下の場合' do
+        fixed_task.detail = 'アイウエオ'
         expect(fixed_task).to be_invalid
       end
     end
@@ -28,7 +28,7 @@ RSpec.describe Task, type: :model do
     let(:task2) { FactoryBot.create(:task2) }
     let(:params) { { 'name' => name, 'progress' => progress } }
     context 'when params is nil' do
-      context "nilで検索した場合全てのデータを返す" do
+      context 'nilで検索した場合全てのデータを返す' do
         it 'return task1' do
           expect(Task.search(nil)).to include(task1, task2)
         end
@@ -36,45 +36,46 @@ RSpec.describe Task, type: :model do
     end
   end
 
-  describe "search" do
-    let(:task1) {FactoryBot.create(:task1)} 
-    let(:task2) {FactoryBot.create(:task2)}
-    let(:params) { {"name" => name, "progress" => progress, "priority" => priority } } 
+  describe 'search' do
+    let(:task1) { FactoryBot.create(:task1) }
+    let(:task2) { FactoryBot.create(:task2) }
+    let(:params) { { 'name' => name, 'progress' => progress, 'priority' => priority } }
 
-    context "when params is nil" do
-        context "nilで検索した場合全てのデータを返す" do
-            it "return task1" do
-                expect(Task.search(nil)).to include(task1,task2)
-            end
+    context 'when params is nil' do
+      context 'nilで検索した場合全てのデータを返す' do
+        it 'return task1' do
+          expect(Task.search(nil)).to include(task1, task2)
         end
+      end
     end
 
-    context "when params exist" do
-        context "トレーニングで検索して該当の値を返す" do
-            let(:name) { "トレーニング" } 
-            let(:progress) { "" }
-            let(:priority) { "" } 
-            it "return task1" do
-                expect(Task.search(params)).to include(task1)
-            end
+    context 'when params exist' do
+      context 'トレーニングで検索して該当の値を返す' do
+        let(:name) { 'トレーニング' }
+        let(:progress) { '' }
+        let(:priority) { '' }
+        it 'return task1' do
+          expect(Task.search(params)).to include(task1)
         end
+      end
 
-        context "未着手で検索して値を返す" do
-            let(:name) { "" } 
-            let(:progress) { "WORKING" } 
-            let(:priority) { "" } 
-            it "return task2" do
-                expect(Task.search(params)).to include(task2)
-            end
+      context '未着手で検索して値を返す' do
+        let(:name) { '' }
+        let(:progress) { 'WORKING' }
+        let(:priority) { '' }
+        it 'return task2' do
+          expect(Task.search(params)).to include(task2)
         end
+      end
 
-        context "優先度HIGHで検索して値を返す" do
-            let(:name) { "" } 
-            let(:progress) { "" } 
-            let(:priority) { "HIGH" } 
-            it "return task1" do
-                expect(Task.search(params)).to include(task1)
+      context '優先度HIGHで検索して値を返す' do
+        let(:name) { '' }
+        let(:progress) { '' }
+        let(:priority) { 'HIGH' }
+        it 'return task1' do
+          expect(Task.search(params)).to include(task1)
         end
+      end
     end
   end
 end
