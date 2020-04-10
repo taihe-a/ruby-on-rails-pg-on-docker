88b78211934e0fae4b24a64c2fd352685b0a12f5
taihe-a <taihei618@gmail.com>
2020-04-10 19:23:04 +0900

    admin雛形作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
