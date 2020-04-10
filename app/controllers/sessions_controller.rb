8c132b91f611fdd6655d8d616c9fb0805f7f19d6
taihe-a <taihei618@gmail.com>
2020-04-10 15:03:53 +0900

    ログインまで

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
-    binding.pry
-    
     user = User.find_by(email: session_params[:email])
+          
+    binding.pry
+      
 
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
--- db/dummy_data.rb
+++ db/dummy_data.rb
@@ -7,7 +7,7 @@ def tasks
       detail: "タスク詳細表示タスク詳細表示#{i}",
       deadline: Faker::Date.between(from: Time.zone.today, to: 1.month.ago),
       progress: Faker::Number.between(from: 1, to: 3),
-      user_id: Faker::Number.between(from: 1, to: 3),
+      user_id: Faker::Number.between(from: 4, to: 6),
       priority: Faker::Number.between(from: 1, to: 3)
     )
   end
