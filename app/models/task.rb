6530fc887f30c9ed7143ee997686405702fe823b
taihe-a <taihei618@gmail.com>
2020-04-14 17:59:35 +0900

    labelモデルの作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
+  has_many :labellings, dependent: :destroy
+  has_many :labels, through: :labellings
   validates :name, presence: true
   validates :detail, length: { minimum: 10 }
 
