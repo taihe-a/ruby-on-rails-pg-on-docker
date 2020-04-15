1e45bdc173923787ed8b419266679a60ec533a9f
taihe-a <taihei618@gmail.com>
2020-04-15 17:17:03 +0900

    fix/rubocop

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,9 +3,9 @@
 class TasksController < ApplicationController
   def index
     @task = if params[:direction] == 'DESC'
-               Task.page(params[:page]).per(15).preload(:labels).where(user_id: current_user).order(deadline: 'DESC')
-             else
-               Task.page(params[:page]).per(15).preload(:labels).where(user_id: current_user.id).order(deadline: 'ASC')
+              Task.page(params[:page]).per(15).preload(:labels).where(user_id: current_user).order(deadline: 'DESC')
+            else
+              Task.page(params[:page]).per(15).preload(:labels).where(user_id: current_user.id).order(deadline: 'ASC')
             end
   end
 
